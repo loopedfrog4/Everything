b93db871aa33ae61f5880ac6330e14fe810ad2fa
Zheng Hui <zhenghui39@hotmail.com>
2022-02-02 19:51:23 +0800

    Deleted

--- Tutorial_2/question5.py
+++
@@ -1,6 +0,0 @@
-# Write a program that reads in a sequence of characters, 
-# and determines whether its parentheses, braces, and curly braces are "balanced." 
-# Your program should read one line of input containing what is supposed to be a properly-formed 
-# expression in algebra and tells whether it is in fact legal. The expression could have 
-# several sets of grouping symbols of various kinds, () and [] and {}. 
-# Your program needs to make sure that these grouping symbols match up properly.
