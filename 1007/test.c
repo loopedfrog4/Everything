b503cceacd3620bb2be06969eb11a454d57fb85a
Zheng Hui <zhenghui39@hotmail.com>
2022-02-02 22:04:05 +0800

    Updated

---
+++ 1007/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int a = 1;
+   printf("%d", a);
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   return 0;
+}
