4041b604258b3480aa44a89e535d477eb02e4cdb
Zheng Hui <zhenghui39@hotmail.com>
2022-02-27 17:28:14 +0800

    Updated

--- 1008/Tutorial_5/N Queen.py
+++ 1008/Tutorial_5/N Queen.py
@@ -1,6 +1,7 @@
+# n-queen problem
 def isValid(row, col, chessBoard):
-    N = len(chessBoard)
-    for i in range(N):
+    n = len(chessBoard)
+    for i in range(row):
         if chessBoard[i][col] == 1:
             return False
 
@@ -9,18 +10,16 @@ def isValid(row, col, chessBoard):
     while (r >= 0 and c >= 0):
         if chessBoard[r][c] == 1:
             return False
-        else:
-            r -= 1
-            c -= 1
+        r -= 1
+        c -= 1
     
     r = row - 1
     c = col + 1
-    while (r >= 0 and c < N):
+    while (r >= 0 and c < n):
         if chessBoard[r][c] == 1:
             return False
-        else:
-            r -= 1
-            c += 1
+        r -= 1
+        c += 1
     return True
 
 def printSolution(chessBoard):
@@ -28,24 +27,22 @@ def printSolution(chessBoard):
     for i in range(len(chessBoard)):
         print(chessBoard[i])
 
-def putAQueen(row, chessBoard):
+def putAQueen(row,chessBoard):
     N = len(chessBoard)
+
     for col in range(N):
-        if isValid(row, col, chessBoard):
+        if isValid(row,col, chessBoard):
             chessBoard[row][col] = 1
             if (row < N-1):
-                putAQueen(row+1, chessBoard)
+                putAQueen(row+1,chessBoard)
             else:
                 printSolution(chessBoard)
                 chessBoard[row][col] = 0
                 return
             chessBoard[row][col] = 0
 
-N = 4
-chessBoard = []
-for i in range(N):
-  chessBoard.append([])
-  for j in range(N):
-    chessBoard[i].append(0)
 
-putAQueen(0,chessBoard)
+N = 4
+chessBoard = [[0] * N for i in range(N)]
+
+putAQueen(0, chessBoard)
--- 1008/Week_7_Revision/dynamic_programming.py
+++ 1008/Week_7_Revision/dynamic_programming.py
@@ -1,23 +1,45 @@
-def dp_knapsack(cap,wt,val,n):
-    array = [[0] * (cap+1) for i in range(n+1)] 
+import numpy as np
+
+# 0/1 knapsack
+def dp_knapsack(c, wt, val, n):
+    array = [[0] * (c + 1) for i in range(n+1)]
     
-    for i in range(n, -1, -1): # iterate from right to left
-        for currentCap in range(0, cap+1): # iterate down the rows from Capacity: 0 to Capacity to 10 (so 11 times total)
-            if currentCap < wt[i-1]: # if current capacity of truck is less than currently considered item
-                array[i-1][currentCap] = array[i][currentCap] # cannot take item, so take state of the right side
-            else: # capacity is equal or bigger than the currently considered item, so take OR don't take
-                array[i-1][currentCap] = max(array[i][currentCap], val[i-1] + array[i][currentCap - wt[i-1]])
-                #                        ^ don't take the currently considered item so take state at right
-                #                                                  ^ take ( currently considered item + the state of right after removing the weight of the currently considered item)
-    return array[0][cap]
+    for i in range(n,-1,-1):
+        for currentcap in range(c+1):
+            if currentcap < wt[i-1]:
+                array[i-1][currentcap] = array[i][currentcap]
+            else:
+                array[i-1][currentcap] = max(array[i][currentcap], val[i-1] + array[i][currentcap - wt[i-1]])
+    return array[0][c]
 
+def naive_knapsack(c,wt,val,n):
+    density = np.array(val) / np.array(wt)
+    
+    t_sort = np.argsort(density)[::-1]
+    density = np.array(density)[t_sort]
+    wt = np.array(wt)[t_sort]
+    val = np.array(val)[t_sort]
+    
+    return_val = 0
+    index = 0
 
-# capacity = 10
-# val = [4,3,10]
-# wt = [5,3,5]
+    while c > 0:
+        if c >= wt[index]: #if the current(and the whole of) item in the space can considered to add into the truck
+            c -= wt[index]
+            return_val += val[index]
+        else:
+            return_val += c * density[index]
+            c = 0
+        index += 1
+    return return_val
+    
+# capacity = 50
+# val = [60, 100, 120]
+# wt = [10, 20, 30]
 
-capacity = 50
-val = [60, 100, 120]
-wt = [10, 20, 30]
+capacity = 10
+val = [4,3,10]
+wt = [5,3,5]
 
-dp_knapsack(capacity, wt, val, len(val))
+# print(dp_knapsack(capacity, wt, val, len(wt)))
+print(naive_knapsack(capacity,wt,val,len(wt)))
