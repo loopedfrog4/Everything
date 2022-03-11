5a2e369aca48ffcfab5cc8bcb16fec82889e2608
Zheng Hui <zhenghui39@hotmail.com>
2022-03-11 13:49:30 +0800

    Updated

--- 1007/Group_Assignment/Group_Assignment.c
+++ 1007/Group_Assignment/Group_Assignment.c
@@ -1,47 +1,77 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define numberOfProcesses 5
 
 struct process{
     int processNum;
     int arrivalTime;
     int burstTime;
-};
-
-struct process process_array[numberOfProcesses];
-
-int burstTime[numberOfProcesses];
-int arrivalTime[numberOfProcesses];
+    int waitTime;
+    int turnAroundTime;
+    float ratio;
+    int completed; // 1 = Completed, 0 = Pending
+}typedef process;
 
 float avgTurnAroundTime = 0.0;
 float maxTurnAroundTime = 0.0;
 float avgWaitTime = 0.0;
 float maxWaitTime = 0.0;
 
+// Sort processes based on arrival time
+void sortByArrivalTime(process arr[]){
+    int i, j;
+
+    process temp;
+    
+    // Selection Sort
+    for (i = 0; i < numberOfProcesses - 1; i++) {
+        for (j = i + 1; j < numberOfProcesses; j++) {
+ 
+            // Check for lesser arrival time
+            if (arr[i].arrivalTime > arr[j].arrivalTime) {
+ 
+                // Swap earlier process to front
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 int main () {
+    FILE* fp;
+    char  line[10];
+    int count = 1;
+    int i = 0;
+    process process_arr[numberOfProcesses];
 
-
-    int c;
-    FILE *file;
-    file = fopen("testcase1.txt", "r");
-    // if (file) {
-    //     while ((c = getc(file)) != EOF)
-    //         fscanf(file, "%d %d\n", &process_array[i].burstTime, &process_array[i].arrivalTime);
-    //         printf("%d %d\n", process_array[i].burstTime, process_array[i].arrivalTime);
-    //     fclose(file);
-    // }
-
-    int temp;
-    fscanf(file, "%d", &temp);
-    //Now scan all the line inside the text
-    int i;
-    for (i = 0; i < numberOfProcesses; i++) {
-        fscanf(file, "%d %d\n", &process_array[i].burstTime, &process_array[i].arrivalTime);
-        
+    fp = fopen("testcase3.txt" , "r");
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        const char* arrival = strtok(line, " ");
+        const char* burst = strtok(NULL, " ");
+        // printf("P %d\n Arrival Time %s\n Burst Time %s\n", count, arrival, burst);
+        // printf("\n");
+        process_arr[i].processNum = count;
+        process_arr[i].arrivalTime = atoi(arrival);
+        process_arr[i].burstTime = atoi(burst);
+        count += 1;
+        i += 1;
     }
     
+    // Call helper method to sort array of processes based on arrival time
+    sortByArrivalTime(process_arr);
     
-    printf("%d", process_array[0].arrivalTime);
-    printf("%d", process_array[0].burstTime);
+    // Print All Processes
+    for (int z = 0; z < numberOfProcesses; z++){
+        printf("P%d\n", process_arr[z].processNum);
+        printf("Arrival: %d\n", process_arr[z].arrivalTime);
+        printf("Burst: %d\n", process_arr[z].burstTime);
+        printf("\n");
+    }
 
     return 0;
 }
